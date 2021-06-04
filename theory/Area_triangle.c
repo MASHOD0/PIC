0c068daa8763980e095cb282c318ab2b94c91fd9
MASHOD0 <mashhood2002@gmail.com>
2021-06-04 10:31:35 +0530

    added Area_triangle.c

---
+++ theory/Area_triangle.c
@@ -0,0 +1,15 @@
+int main()
+{
+    int height;
+    int breadth;
+   
+    printf("what is height of triangle of the triangle\n");
+    scanf("%d", &height);
+
+    printf("what is base of triangle of the triangle\n");
+    scanf("%d", &breadth);
+
+    printf("the area is givem as %d", 0.5 * height * breadth);
+    
+    return 0;
+}
