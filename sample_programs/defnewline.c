8a660f5d2190fb194f67d5dcea3ad9e48acc1e1f
MASHOD0 <mashhood2002@gmail.com>
2021-06-25 11:53:54 +0530

    added defnewline.c

---
+++ sample_programs/defnewline.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#define LENGTH 10
+#define WIDTH 5
+#define NEWLINE "\n"
+void main()
+{
+int area;
+area=LENGTH * WIDTH;
+printf("area = %d", area);
+printf("%c", NEWLINE);
+printf("Thank you");
+}
